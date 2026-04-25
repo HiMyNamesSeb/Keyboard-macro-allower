364b0a4012ac3e9d0e89faf6af422b5ca9ba0935
HiMyNamesSeb <110443934+HiMyNamesSeb@users.noreply.github.com>
2026-04-25 02:01:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,68 @@
-import json
-import time
+#----------------------------------------------------- imports
+import record
+import playback
+import threading
+
+#----------------------------------------------------- setup pynput and listeners
 from pynput import keyboard, mouse
+m_listener = mouse.Listener(on_click=record.on_mouse_click , on_move=record.on_mouse_move)
+k_listener = keyboard.Listener(on_press=record.on_key_press)
+m_listener.start()
+k_listener.start()
 
-TOGGLE_KEY = keyboard.Key.f8
+#----------------------------------------------------- setup customtkinter app window
+import customtkinter
+app = customtkinter.CTk()
+
+#----------------------------------------------------- variables
+#customize button keybinds in record.py
+isLooping = False
 OUTPUT_FILE = "recording.json"
+#----------------------------------------------------- Start Recording button
+def startRecordingEvent(): #
+    record.flipIsRecording()
+    record.clear_recordings()
+    print("you are now recording")
+startRecording = customtkinter.CTkButton(master= app, command = startRecordingEvent , text = "start recording" )
+startRecording.pack()
 
-flipKey = "["
-isRecording = False
-events = []
+#----------------------------------------------------- End Recording button
 
-def on_key_press(key):
-    global isRecording
-    keyData = str(key)
-    keyData = keyData.replace("'", "")
-    if keyData == flipKey:
-        if isRecording:
-            print("\n you have stopped recording")
-            isRecording = False
-            save_recording()
-        elif not isRecording:
-            isRecording = True
-            print("\n you are now recording")
-            return
-    if not isRecording:
-        return
-    if keyData == 'Key.space':
-        keyData = ' '
-    if keyData == 'Key.shift_r' or keyData == 'Key.shift':
-        keyData = ''
-    if keyData == 'Key.enter':
-        keyData = '\n'
+def endRecordingEvent():
+    record.flipIsRecording()
+    record.save_recording()
+    print("you stopped recording")
+endRecording = customtkinter.CTkButton(master= app, command = endRecordingEvent , text = "stop recording" )
+endRecording.pack()
 
-    events.append({
-        "type": "key",
-        "key": keyData,
-        "time": time.time()
-    })
+#----------------------------------------------------- Single Playback button
+def singlePlaybackEvent():
+    playback.playback(playback.load_recording())
+singlePlayback = customtkinter.CTkButton(master= app, command = singlePlaybackEvent , text = "playback" )
+singlePlayback.pack()
 
-def on_mouse_click(x, y, button, pressed):
+#----------------------------------------------------- Loop Playback button
+def loopPlaybackEvent():
+    global isLooping
+    isLooping = True
+    t = threading.Thread(target=loop_playback)
+    t.daemon = True  # thread dies when the app closes
+    t.start()
+loopPlayback = customtkinter.CTkButton(master= app, command = loopPlaybackEvent , text = "start loop" )
+loopPlayback.pack()
 
-    global isRecording
-    if not isRecording:
-        return
-    events.append({
-        "type": "mouse",
-        "x": x,
-        "y": y,
-        "button": str(button),
-        "pressed": pressed,
-        "time": time.time()
-    })
+#----------------------------------------------------- playback loop logic functions
+def loop_playback():
+    record.stop_event.clear()  # reset it before starting
+    print("starting loop")
+    while not record.stop_event.is_set():
+        playback.playback(playback.load_recording())
 
-    pass
-
-def save_recording():
-    # YOUR JOB: write events list to OUTPUT_FILE as JSON
-    with open(OUTPUT_FILE, "w") as f:
-        json.dump(events, f, indent=2)
-    pass
-
-# Start both listeners
-with mouse.Listener(on_click=on_mouse_click) as m_listener, \
-     keyboard.Listener(on_press=on_key_press) as k_listener:
-    k_listener.join()
-
-
+#----------------------------------------------------- Small customtikinter details
+customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_appearance_mode("dark")
+app.geometry("200x120")
+app.attributes("-topmost", True)
+app.title("Macro Overlay")
+app.resizable(width=False, height=False)
+app.mainloop() #launch the window
